63d60bf4f802e5130ad0f109498004b7a866577f
Talisa Erazo <talisaerazo@gmail.com>
2018-10-24 22:37:53 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -6,7 +6,7 @@ def rows
   puts "   |   |   "
 end
 
-def display_board(board = [" ", " ", " ", " ", " ", " ", " ", " ", " "])
+def display_board(board)
   puts " #{board[0]} | #{board[1]} | #{board[2]} "
   puts "-----------"
   puts " #{board[3]} | #{board[4]} | #{board[5]} "
